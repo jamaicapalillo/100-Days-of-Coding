629c5e130f1458540b0c77a4b13c0cef6b0cf1cf
Jamaica C. Palillo <150937686+jamaicapalillo@users.noreply.github.com>
2024-10-27 16:39:17 +0800

    Update [Main-Project] Caesar_Cipher.py

--- [Beginner-Programs]/[Day-08] - Caesar Cipher/[Main-Project] Caesar_Cipher.py
+++ [Beginner-Programs]/[Day-08] - Caesar Cipher/[Main-Project] Caesar_Cipher.py
@@ -1,13 +1,14 @@
+import time
+
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# TODO 1: Create a function called 'encrypt' that takes the 'original_text' and 'shift' as inputs.
-# TODO 2: Inside the 'encrypt ()' function, shift each letter of the 'original_text' by the 'shift' amount and print the encrypted text.
-# TODO 3: Call the 'encrypt()' function and pass the 'user inputs'. You should be able to test the code and encrypt a message.
-
+def loading_sign (duration = 3, delay=0.5):
+    print ("Exiting", end="")
+    for _ in range (duration):
+        print (".", end="", flush=True)
+        time.sleep(delay)
+    print()
+    
 def caesar_cipher (text, shift, direction):
     output_text = ""
 
@@ -19,35 +20,23 @@ def caesar_cipher (text, shift, direction):
             output_text += alphabets[new_position]
         else:
             output_text += letter
+    
     print(f"The {direction}d text is {output_text}")
 
-caesar_cipher (text, shift, direction)
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-# def encrypt (original_text, shift):
-#     encrypted_text = ""
+    caesar_cipher (text, shift, direction)
 
-#     for letter in original_text:
-#         if letter in alphabets:
-#             new_position = (alphabets.index(letter)+ shift) % 26
-#             encrypted_text += alphabets[new_position]
-#         else:
-#             encrypted_text += letter
+    while True:
+        another_input = input ("Do you want to use the program again? Y or N. ").upper()
 
-#     print(f"The encoded text is {encrypted_text}")
-
-# def decipher (original_text, shift):
-#     deciphered_text = ""
-
-#     for letter in original_text:
-#         if letter in alphabets:
-#             new_position = (alphabets.index(letter) - shift) % 26
-#             deciphered_text += alphabets[new_position]
-#         else:
-#             deciphered_text += letter
-
-#     print(f"The decoded text is {deciphered_text}")
-
-# if direction == "encode":
-#     encrypt (text, shift)
-# elif direction == "decode":
-#     decipher (text, shift)
+        if another_input == "Y":   
+            break
+        elif another_input == "N":
+            print ("Thank you for using our program.")
+            loading_sign()
+        else:
+            ("Invalid. Please type only Y or N.")
